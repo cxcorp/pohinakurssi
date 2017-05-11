23b1f84f7c6485d3843f083aae95e8b2f71b9483
cxcorp <joona.o.heikkila@gmail.com>
2017-05-11 21:20:22 +0300

    Let's try inotify for directory watching

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import * as express from 'express';
 import * as dotenv from 'dotenv';
+import { Inotify } from 'inotify';
 import createLogger from './logger';
 const logger = createLogger(__filename);
-
 dotenv.config();
+
+const inotify = new Inotify();
+
+if (process.env === 'development') {
+    process.once('SIGUSR2', () => {
+        inotify.close();
+        process.kill(process.pid, 'SIGUSR2');
+    });
+}
 
 const app = express();
 app.get('/', (req, res) => {
@@ -11,5 +20,16 @@ app.get('/', (req, res) => {
 
 });
 app.listen(1235, () => {
+
+    const wd = inotify.addWatch({
+        path: './',
+        watch_for: Inotify.IN_CREATE,
+        callback: event => {
+            logger.info('Event! ', event);
+            inotify.close();
+        }
+    });
+
+    logger.info('wd ' + wd);
     logger.info('Server started on port 1235');
 });
